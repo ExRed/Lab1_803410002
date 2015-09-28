29e781d94bcbda99b5f8103227683fb08fd3260e
Pierre Pascal Lindenberg <PPLindenberg@gmail.com>
2015-09-28 12:40:43 +0800

    added .R file with some example instructions

---
+++ Lab1.R
@@ -0,0 +1,10 @@
+x <- 123
+print(x)
+print("Zehn Zahme Ziegen ziehen zehn Zentner Zucker zum Zug!")
+x = 1*1+2*2+3*3
+x
+x <- 1*1+2*2+3*3
+x
+print(sample(c(2,5,3), size=3, replace=TRUE))
+print("Is this sufficient? :P")
+
