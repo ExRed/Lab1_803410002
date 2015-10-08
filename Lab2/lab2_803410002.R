fce29bba9e5f115fc4f023d4c9e8012872a1553e
PPLindenberg@gmail.com <PPLindenberg@gmail.com>
2015-10-08 11:02:00 +0800

    created lab2_803410002.R that calls the part* files

--- Lab2/lab2_803410002.R
+++ Lab2/lab2_803410002.R
@@ -1,62 +1,26 @@
 #Part1 vector (10%)
-#(a) should return 26852735
-# Type your code here
-
-
-
-#(b) should return 2129170437
-# Type your code here
+source("Part1_Vector.R")
 
 
 #part2 matrices (10%)
-# Type your code here
+source("Part2_Matrices.R")
 
-
-
-#The brackets round the first line ensure the matrix ( tmp<-matrix()) is displayed so that we can check that it has been
-#entered correctly.
-#using multiplication (*)
-
-
-# or using crossprod 
 
 
 #part3 (80%)
+source("Part3_DataFrame.R")
 
 # create variable speed, occupancy, volume with c()
-
-
-
 # create variable 'dtbind'  that combines speed, occupancy and volume using cbind() 
-
 #Then check whether 'dtbind' is a matrix
-
 #create data frame from the variable that you have created above, store to a new variable, called  "dtfrm" 
-
-
 #read dataset use read.table() 
 # before that, type getwd() to show your current working directory path and setwd("your file path") to set your path
-
-
 #read dataset use read.csv()
-
-
 #check your data correctness using names(), str(), dim(), head()
-
-
 #why dimenssion dtfrm1 and dtfrm2 different?
-
 #Compute the following: how many observations were made ?
-
-
 #compute mean 'speed', mean'volume' without removing missing value NA (without na.rm=TRUE)
-
-
 #Create new dataset called 'trafficData' with removing NA 
 #na.omit(your.data.frame)
-
-
 #compute mean 'speed', mean 'volume' again (in dataset trafficData) 
-
-
-
