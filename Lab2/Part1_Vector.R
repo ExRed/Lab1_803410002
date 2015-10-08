030ed685457ade27c90ab6c2d015cb34357d99f6
PPLindenberg@gmail.com <PPLindenberg@gmail.com>
2015-10-08 09:31:17 +0800

    Competed Part1_Vectors a) and b)

---
+++ Lab2/Part1_Vector.R
@@ -0,0 +1,10 @@
+i <- c(10:100)
+result <- sum (i*i*i + 4*i*i)
+print("a) sum from i=10 to 100 of i^3 + 4i^2 = ")
+print(result)
+
+i <- c(1:25)
+result <- sum( ((2^i)/i) + ((3^i)/i^2) )
+print("b) sum from i=1 to 25 of ( ((2^i)/i) + ((3^i)/i^2) ) = ")
+print(result)
+
