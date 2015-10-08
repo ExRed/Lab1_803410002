46b304b53f4d3c6d76fa0c4c22e78c8e3d2c768d
PPLindenberg@gmail.com <PPLindenberg@gmail.com>
2015-10-08 11:01:06 +0800

    prettyfication and following the template

--- Lab2/Part1_Vector.R
+++ Lab2/Part1_Vector.R
@@ -1,10 +1,10 @@
+#(a) should return 26852735
 i <- c(10:100)
 result <- sum (i*i*i + 4*i*i)
-print("a) sum from i=10 to 100 of i^3 + 4i^2 = ")
 print(result)
 
+
+#(b) should return 2129170437
 i <- c(1:25)
 result <- sum( ((2^i)/i) + ((3^i)/i^2) )
-print("b) sum from i=1 to 25 of ( ((2^i)/i) + ((3^i)/i^2) ) = ")
 print(result)
-
